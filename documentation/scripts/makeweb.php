d3cc9ac89eadeae4cefc56609da2afbdf5187f05
monte.ohrt <monte.ohrt@9dce5a81-9a42-0410-99e4-3799d3902e7f>
2010-11-16 14:53:03 +0000

    update docs

--- documentation/scripts/makeweb.php
+++ documentation/scripts/makeweb.php
@@ -27,7 +27,7 @@ foreach (new RecursiveIteratorIterator($dhandle) as $fpath) {
         $body = str_replace(array('{','}','@@LDELIM@@','@@RDELIM@@'),array('@@LDELIM@@','@@RDELIM@@','{ldelim}','{rdelim}'),$body);
         $title = str_replace(array('{','}','@@LDELIM@@','@@RDELIM@@'),array('@@LDELIM@@','@@RDELIM@@','{ldelim}','{rdelim}'),$title);
         // fix link filenames, remove .tpl
-	$body = preg_replace('!href="(.*)\.tpl(#.*)?"!','href="$1$2"',$body);
+	//$body = preg_replace('!href="(.*)\.tpl(#.*)?"!','href="$1$2"',$body);
         $template = "{extends file='layout.tpl'}\n{block name=title}@TITLE@{/block}{block name=main_content}@BODY@{/block}";
         $template = str_replace(array('@TITLE@','@BODY@'),array($title,$body),$template);
 	if(!is_dir($webdir))
