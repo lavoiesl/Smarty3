4a0da355f7aa72fd64863e62e1e000ec7b5ad1ee
monte.ohrt <monte.ohrt@9dce5a81-9a42-0410-99e4-3799d3902e7f>
2010-11-28 23:08:48 +0000

    fix sentence

--- documentation/scripts/makeweb.php
+++ documentation/scripts/makeweb.php
@@ -26,10 +26,16 @@ foreach (new RecursiveIteratorIterator($dhandle) as $fpath) {
 	$body = $match[1];
         $body = str_replace(array('{','}','@@LDELIM@@','@@RDELIM@@'),array('@@LDELIM@@','@@RDELIM@@','{ldelim}','{rdelim}'),$body);
         $title = str_replace(array('{','}','@@LDELIM@@','@@RDELIM@@'),array('@@LDELIM@@','@@RDELIM@@','{ldelim}','{rdelim}'),$title);
+        $description = $title;
+        $keywords = preg_replace(array('![^\w\s]+!','!\s+!'),array(' ',', '),strtolower(trim($title)));
         // fix link filenames, remove .tpl
 	//$body = preg_replace('!href="(.*)\.tpl(#.*)?"!','href="$1$2"',$body);
-        $template = "{extends file='layout.tpl'}\n{block name=title}@TITLE@{/block}{block name=main_content}@BODY@{/block}";
-        $template = str_replace(array('@TITLE@','@BODY@'),array($title,$body),$template);
+        $template = "{extends file='layout.tpl'}\n";
+        $template .= "{block name=title}@TITLE@{/block}\n";
+        $template .= "{block name=description}@DESCRIPTION@{/block}\n";
+        $template .= "{block name=keywords}@KEYWORDS@{/block}\n";
+        $template .= "{block name=main_content}@BODY@{/block}\n";
+        $template = str_replace(array('@TITLE@','@BODY@','@DESCRIPTION@','@KEYWORDS@'),array($title,$body,$description,$keywords),$template);
 	if(!is_dir($webdir))
           mkdir($webdir,0755,true);
         file_put_contents($webdir.'/'.$fname,$template);
