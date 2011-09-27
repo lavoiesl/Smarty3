cfab28d0874e7a03fba187e5a7dff2e8d65f012c
uwe.tews@googlemail.com <uwe.tews@googlemail.com@9dce5a81-9a42-0410-99e4-3799d3902e7f>
2011-09-27 21:48:33 +0000

    - bugfix wrong resource could be used on compile check of custom resource

--- development/PHPunit/ObjectVariableTests.php
+++ development/PHPunit/ObjectVariableTests.php
@@ -1,9 +1,9 @@
 <?php
 /**
 * Smarty PHPunit tests object variables
-* 
+*
 * @package PHPunit
-* @author Uwe Tews 
+* @author Uwe Tews
 */
 
 /**
@@ -15,12 +15,12 @@ class ObjectVariableTests extends PHPUnit_Framework_TestCase {
         $this->smarty = SmartyTests::$smarty;
         SmartyTests::init();
         $this->smarty->force_compile = true;
-    } 
+    }
 
     public static function isRunnable()
     {
         return true;
-    } 
+    }
 
     /**
     * test simple object variable
@@ -31,7 +31,39 @@ class ObjectVariableTests extends PHPUnit_Framework_TestCase {
         $tpl = $this->smarty->createTemplate('string:{$object->hello}');
         $tpl->assign('object', $object);
         $this->assertEquals('hello_world', $this->smarty->fetch($tpl));
-    } 
+    }
+    /**
+    * test simple object variable with variable property
+    */
+    public function testObjectVariableOutputVariableProperty()
+    {
+        $object = new VariableObject;
+        $this->smarty->disableSecurity();
+        $tpl = $this->smarty->createTemplate('string:{$p=\'hello\'}{$object->$p}');
+        $tpl->assign('object', $object);
+        $this->assertEquals('hello world', $this->smarty->fetch($tpl));
+    }
+    /**
+    * test simple object variable with method
+    */
+    public function testObjectVariableOutputMethod()
+    {
+        $object = new VariableObject;
+        $tpl = $this->smarty->createTemplate('string:{$object->myhello()}');
+        $tpl->assign('object', $object);
+        $this->assertEquals('hello_world', $this->smarty->fetch($tpl));
+    }
+    /**
+    * test simple object variable with method
+    */
+    public function testObjectVariableOutputVariableMethod()
+    {
+        $object = new VariableObject;
+        $this->smarty->disableSecurity();
+        $tpl = $this->smarty->createTemplate('string:{$p=\'myhello\'}{$object->$p()}');
+        $tpl->assign('object', $object);
+        $this->assertEquals('hello world', $this->smarty->fetch($tpl));
+    }
     /**
     * test  object variable in double quoted string
     */
@@ -41,7 +73,7 @@ class ObjectVariableTests extends PHPUnit_Framework_TestCase {
         $tpl = $this->smarty->createTemplate('string:{"double quoted `$object->hello` okay"}');
         $tpl->assign('object', $object);
         $this->assertEquals('double quoted hello_world okay', $this->smarty->fetch($tpl));
-    } 
+    }
     /**
     * test  object variable in double quoted string as include name
     */
@@ -51,16 +83,16 @@ class ObjectVariableTests extends PHPUnit_Framework_TestCase {
         $tpl = $this->smarty->createTemplate('string:{include file="`$object->hello`_test.tpl"}');
         $tpl->assign('object', $object);
         $this->assertEquals('hello world', $this->smarty->fetch($tpl));
-    } 
-} 
+    }
+}
 
 Class VariableObject {
     public $hello = 'hello_world';
 
-    function myhello($params)
+    function myhello()
     {
         return 'hello world';
-    } 
-} 
+    }
+}
 
 ?>
