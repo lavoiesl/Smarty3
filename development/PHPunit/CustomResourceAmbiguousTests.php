130d5adc40c1abe199e8bbe16162bb83f3ae1398
rodneyrehm <rodneyrehm@9dce5a81-9a42-0410-99e4-3799d3902e7f>
2011-10-13 13:10:06 +0000

    - add concept unique_resource to combat potentially ambiguous template_resource values when custom resource handlers are used (Forum Topic 2012)
    http://www.smarty.net/forums/viewtopic.php?t=2012
    
    thereby DRYed and optimized the resource handler identification method.

---
+++ development/PHPunit/CustomResourceAmbiguousTests.php
@@ -0,0 +1,90 @@
+<?php
+/**
+* Smarty PHPunit tests for File resources
+*
+* @package PHPunit
+* @author Uwe Tews
+*/
+
+
+/**
+* class for file resource tests
+*/
+class CustomResourceAmbiguousTests extends PHPUnit_Framework_TestCase {
+    public $_resource = null;
+    
+    public function setUp()
+    {
+        $this->smarty = SmartyTests::$smarty;
+        SmartyTests::init();
+        
+        // empty the template dir
+        $this->smarty->setTemplateDir(array());
+        
+        // load and register custom resource handler
+        require_once dirname(__FILE__) . '/PHPunitplugins/resource.ambiguous.php';
+        $this->_resource = new Smarty_Resource_Ambiguous(dirname(__FILE__) . '/templates/ambiguous/');
+        $this->smarty->registerResource('ambiguous', $this->_resource);
+        $this->smarty->default_resource_type = 'ambiguous';
+    }
+
+    public static function isRunnable()
+    {
+        return true;
+    }
+
+    protected function relative($path)
+    {
+        $path = str_replace( dirname(__FILE__), '.', $path );
+        if (DS == "\\") {
+            $path = str_replace( "\\", "/", $path );
+        }
+        return $path;
+    }
+
+    public function testNone()
+    {
+        $tpl = $this->smarty->createTemplate('foobar.tpl');
+        $this->assertFalse($tpl->source->exists);
+    }
+    
+    public function testCase1()
+    {
+        $this->_resource->setSegment('case1');
+        $tpl = $this->smarty->createTemplate('foobar.tpl');
+        $this->assertTrue($tpl->source->exists);
+        $this->assertEquals('case1', $tpl->source->content);
+    }
+    
+    public function testCase2()
+    {
+        $this->_resource->setSegment('case2');
+        $tpl = $this->smarty->createTemplate('foobar.tpl');
+        $this->assertTrue($tpl->source->exists);
+        $this->assertEquals('case2', $tpl->source->content);
+    }
+    
+    public function testCaseSwitching()
+    {
+        $this->_resource->setSegment('case1');
+        $tpl = $this->smarty->createTemplate('foobar.tpl');
+        $this->assertTrue($tpl->source->exists);
+        $this->assertEquals('case1', $tpl->source->content);
+        
+        $this->_resource->setSegment('case2');
+        $tpl = $this->smarty->createTemplate('foobar.tpl');
+        $this->assertTrue($tpl->source->exists);
+        $this->assertEquals('case2', $tpl->source->content);
+    }
+
+    /**
+    * final cleanup
+    */
+    public function testFinalCleanup()
+    {
+        $this->smarty->clearCompiledTemplate();
+        $this->smarty->clearAllCache();
+    }
+}
+
+?>
--- development/PHPunit/smartytestssingle.php
+++ development/PHPunit/smartytestssingle.php
@@ -79,7 +79,7 @@ class SmartyTests extends PHPUnit_Framework_TestSuite {
     public static function suite()
     {
         $testorder = array(
-            'PluginFunctionHtmlCheckboxesTests'
+            'CustomResourceAmbiguousTests'
         );
         $smarty_libs_dir = dirname(__FILE__) . '/../../distribution/libs';
         if (method_exists('PHPUnit_Util_Filter', $smarty_libs_dir)) {
