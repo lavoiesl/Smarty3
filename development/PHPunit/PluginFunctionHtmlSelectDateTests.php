a1832ce94779bb583668053946d9aac0609e640f
rodneyrehm <rodneyrehm@9dce5a81-9a42-0410-99e4-3799d3902e7f>
2011-10-20 13:31:25 +0000

    - bugfix {html_select_date} and {html_select_time} did not properly handle empty time arguments (Forum Topic 20190)
    see http://www.smarty.net/forums/viewtopic.php?t=20190

--- development/PHPunit/PluginFunctionHtmlSelectDateTests.php
+++ development/PHPunit/PluginFunctionHtmlSelectDateTests.php
@@ -65,9 +65,22 @@ class PluginFunctionHtmlSelectDateTests extends PHPUnit_Framework_TestCase {
 <option value="2010">2010</option>
 <option value="2011" selected="selected">2011</option>',
         'default' => '<option value="2011" selected="selected">2011</option>',
+        'none' => '<option value="2011">2011</option>',
     );
     
     protected $months = array(
+        'none' => '<option value="01">January</option>
+<option value="02">February</option>
+<option value="03">March</option>
+<option value="04">April</option>
+<option value="05">May</option>
+<option value="06">June</option>
+<option value="07">July</option>
+<option value="08">August</option>
+<option value="09">September</option>
+<option value="10">October</option>
+<option value="11">November</option>
+<option value="12">December</option>',
         'default' => '<option value="01">January</option>
 <option value="02" selected="selected">February</option>
 <option value="03">March</option>
@@ -119,6 +132,37 @@ class PluginFunctionHtmlSelectDateTests extends PHPUnit_Framework_TestCase {
     );
     
     protected $days = array(
+        'none' => '<option value="1">01</option>
+<option value="2">02</option>
+<option value="3">03</option>
+<option value="4">04</option>
+<option value="5">05</option>
+<option value="6">06</option>
+<option value="7">07</option>
+<option value="8">08</option>
+<option value="9">09</option>
+<option value="10">10</option>
+<option value="11">11</option>
+<option value="12">12</option>
+<option value="13">13</option>
+<option value="14">14</option>
+<option value="15">15</option>
+<option value="16">16</option>
+<option value="17">17</option>
+<option value="18">18</option>
+<option value="19">19</option>
+<option value="20">20</option>
+<option value="21">21</option>
+<option value="22">22</option>
+<option value="23">23</option>
+<option value="24">24</option>
+<option value="25">25</option>
+<option value="26">26</option>
+<option value="27">27</option>
+<option value="28">28</option>
+<option value="29">29</option>
+<option value="30">30</option>
+<option value="31">31</option>',
         'default' => '<option value="1">01</option>
 <option value="2">02</option>
 <option value="3">03</option>
@@ -339,6 +383,34 @@ class PluginFunctionHtmlSelectDateTests extends PHPUnit_Framework_TestCase {
             .$n.'<select name="Date_Day">'.$n.'<option value="">day</option>'.$n. $this->days['default'] .$n.'</select>'
             .$n.'<select name="Date_Year">'.$n.'<option value="">year</option>'.$n. $this->years['default'] .$n.'</select>';
         $tpl = $this->smarty->createTemplate('eval:{html_select_date time='. $this->now .' year_empty="year" month_empty="month" day_empty="day"}');
+        $this->assertEquals($result, $this->smarty->fetch($tpl));
+    }
+    
+    public function testEmptyUnset()
+    {
+        $n = "\n";
+        $result = '<select name="Date_Month">'.$n.'<option value=""></option>'.$n. $this->months['none'] .$n.'</select>'
+            .$n.'<select name="Date_Day">'.$n.'<option value=""></option>'.$n. $this->days['none'] .$n.'</select>'
+            .$n.'<select name="Date_Year">'.$n.'<option value=""></option>'.$n. $this->years['none'] .$n.'</select>';
+        $tpl = $this->smarty->createTemplate('eval:{html_select_date time=null all_empty=""}');
+        $this->assertEquals($result, $this->smarty->fetch($tpl));
+        
+        $result = '<select name="Date_Month">'.$n.'<option value="">all</option>'.$n. $this->months['none'] .$n.'</select>'
+            .$n.'<select name="Date_Day">'.$n.'<option value="">all</option>'.$n. $this->days['none'] .$n.'</select>'
+            .$n.'<select name="Date_Year">'.$n.'<option value="">all</option>'.$n. $this->years['none'] .$n.'</select>';
+        $tpl = $this->smarty->createTemplate('eval:{html_select_date time=null all_empty="all"}');
+        $this->assertEquals($result, $this->smarty->fetch($tpl));
+        
+        $result = '<select name="Date_Month">'.$n. $this->months['none'] .$n.'</select>'
+            .$n.'<select name="Date_Day">'.$n. $this->days['none'] .$n.'</select>'
+            .$n.'<select name="Date_Year">'.$n.'<option value=""></option>'.$n. $this->years['none'] .$n.'</select>';
+        $tpl = $this->smarty->createTemplate('eval:{html_select_date time=null year_empty=""}');
+        $this->assertEquals($result, $this->smarty->fetch($tpl));
+        
+        $result = '<select name="Date_Month">'.$n.'<option value="">month</option>'.$n. $this->months['none'] .$n.'</select>'
+            .$n.'<select name="Date_Day">'.$n.'<option value="">day</option>'.$n. $this->days['none'] .$n.'</select>'
+            .$n.'<select name="Date_Year">'.$n.'<option value="">year</option>'.$n. $this->years['none'] .$n.'</select>';
+        $tpl = $this->smarty->createTemplate('eval:{html_select_date time=null year_empty="year" month_empty="month" day_empty="day"}');
         $this->assertEquals($result, $this->smarty->fetch($tpl));
     }
     
--- development/PHPunit/PluginFunctionHtmlSelectTimeTests.php
+++ development/PHPunit/PluginFunctionHtmlSelectTimeTests.php
@@ -25,6 +25,30 @@ class PluginFunctionHtmlSelectTimeTests extends PHPUnit_Framework_TestCase {
     
     protected $now = null;
     protected $hours = array(
+        'none' => '<option value="00">00</option>
+<option value="01">01</option>
+<option value="02">02</option>
+<option value="03">03</option>
+<option value="04">04</option>
+<option value="05">05</option>
+<option value="06">06</option>
+<option value="07">07</option>
+<option value="08">08</option>
+<option value="09">09</option>
+<option value="10">10</option>
+<option value="11">11</option>
+<option value="12">12</option>
+<option value="13">13</option>
+<option value="14">14</option>
+<option value="15">15</option>
+<option value="16">16</option>
+<option value="17">17</option>
+<option value="18">18</option>
+<option value="19">19</option>
+<option value="20">20</option>
+<option value="21">21</option>
+<option value="22">22</option>
+<option value="23">23</option>',
         'default' => '<option value="00">00</option>
 <option value="01">01</option>
 <option value="02">02</option>
@@ -111,6 +135,66 @@ class PluginFunctionHtmlSelectTimeTests extends PHPUnit_Framework_TestCase {
 <option value="023">23</option>',
     );
     protected $minutes = array(
+        'none' => '<option value="00">00</option>
+<option value="01">01</option>
+<option value="02">02</option>
+<option value="03">03</option>
+<option value="04">04</option>
+<option value="05">05</option>
+<option value="06">06</option>
+<option value="07">07</option>
+<option value="08">08</option>
+<option value="09">09</option>
+<option value="10">10</option>
+<option value="11">11</option>
+<option value="12">12</option>
+<option value="13">13</option>
+<option value="14">14</option>
+<option value="15">15</option>
+<option value="16">16</option>
+<option value="17">17</option>
+<option value="18">18</option>
+<option value="19">19</option>
+<option value="20">20</option>
+<option value="21">21</option>
+<option value="22">22</option>
+<option value="23">23</option>
+<option value="24">24</option>
+<option value="25">25</option>
+<option value="26">26</option>
+<option value="27">27</option>
+<option value="28">28</option>
+<option value="29">29</option>
+<option value="30">30</option>
+<option value="31">31</option>
+<option value="32">32</option>
+<option value="33">33</option>
+<option value="34">34</option>
+<option value="35">35</option>
+<option value="36">36</option>
+<option value="37">37</option>
+<option value="38">38</option>
+<option value="39">39</option>
+<option value="40">40</option>
+<option value="41">41</option>
+<option value="42">42</option>
+<option value="43">43</option>
+<option value="44">44</option>
+<option value="45">45</option>
+<option value="46">46</option>
+<option value="47">47</option>
+<option value="48">48</option>
+<option value="49">49</option>
+<option value="50">50</option>
+<option value="51">51</option>
+<option value="52">52</option>
+<option value="53">53</option>
+<option value="54">54</option>
+<option value="55">55</option>
+<option value="56">56</option>
+<option value="57">57</option>
+<option value="58">58</option>
+<option value="59">59</option>',
         'default' => '<option value="00">00</option>
 <option value="01">01</option>
 <option value="02">02</option>
@@ -317,6 +401,66 @@ class PluginFunctionHtmlSelectTimeTests extends PHPUnit_Framework_TestCase {
 <option value="059">59</option>',
     );
     protected $seconds = array(
+        'none' => '<option value="00">00</option>
+<option value="01">01</option>
+<option value="02">02</option>
+<option value="03">03</option>
+<option value="04">04</option>
+<option value="05">05</option>
+<option value="06">06</option>
+<option value="07">07</option>
+<option value="08">08</option>
+<option value="09">09</option>
+<option value="10">10</option>
+<option value="11">11</option>
+<option value="12">12</option>
+<option value="13">13</option>
+<option value="14">14</option>
+<option value="15">15</option>
+<option value="16">16</option>
+<option value="17">17</option>
+<option value="18">18</option>
+<option value="19">19</option>
+<option value="20">20</option>
+<option value="21">21</option>
+<option value="22">22</option>
+<option value="23">23</option>
+<option value="24">24</option>
+<option value="25">25</option>
+<option value="26">26</option>
+<option value="27">27</option>
+<option value="28">28</option>
+<option value="29">29</option>
+<option value="30">30</option>
+<option value="31">31</option>
+<option value="32">32</option>
+<option value="33">33</option>
+<option value="34">34</option>
+<option value="35">35</option>
+<option value="36">36</option>
+<option value="37">37</option>
+<option value="38">38</option>
+<option value="39">39</option>
+<option value="40">40</option>
+<option value="41">41</option>
+<option value="42">42</option>
+<option value="43">43</option>
+<option value="44">44</option>
+<option value="45">45</option>
+<option value="46">46</option>
+<option value="47">47</option>
+<option value="48">48</option>
+<option value="49">49</option>
+<option value="50">50</option>
+<option value="51">51</option>
+<option value="52">52</option>
+<option value="53">53</option>
+<option value="54">54</option>
+<option value="55">55</option>
+<option value="56">56</option>
+<option value="57">57</option>
+<option value="58">58</option>
+<option value="59">59</option>',
         'default' => '<option value="00">00</option>
 <option value="01">01</option>
 <option value="02">02</option>
@@ -621,6 +765,34 @@ class PluginFunctionHtmlSelectTimeTests extends PHPUnit_Framework_TestCase {
             .$n.'<select name="Time_Minute">'.$n.'<option value="">minute</option>'.$n. $this->minutes['default'] .$n.'</select>'
             .$n.'<select name="Time_Second">'.$n.'<option value="">second</option>'.$n. $this->seconds['default'] .$n.'</select>';
         $tpl = $this->smarty->createTemplate('eval:{html_select_time time='. $this->now .' hour_empty="hour" minute_empty="minute" second_empty="second"}');
+        $this->assertEquals($result, $this->smarty->fetch($tpl));
+    }
+    
+    public function testEmptyUnset()
+    {
+        $n = "\n";
+        $result = '<select name="Time_Hour">'.$n.'<option value=""></option>'.$n. $this->hours['none'] .$n.'</select>'
+            .$n.'<select name="Time_Minute">'.$n.'<option value=""></option>'.$n. $this->minutes['none'] .$n.'</select>'
+            .$n.'<select name="Time_Second">'.$n.'<option value=""></option>'.$n. $this->seconds['none'] .$n.'</select>';
+        $tpl = $this->smarty->createTemplate('eval:{html_select_time time=null all_empty=""}');
+        $this->assertEquals($result, $this->smarty->fetch($tpl));
+        
+        $result = '<select name="Time_Hour">'.$n.'<option value="">all</option>'.$n. $this->hours['none'] .$n.'</select>'
+            .$n.'<select name="Time_Minute">'.$n.'<option value="">all</option>'.$n. $this->minutes['none'] .$n.'</select>'
+            .$n.'<select name="Time_Second">'.$n.'<option value="">all</option>'.$n. $this->seconds['none'] .$n.'</select>';
+        $tpl = $this->smarty->createTemplate('eval:{html_select_time time=null all_empty="all"}');
+        $this->assertEquals($result, $this->smarty->fetch($tpl));
+
+        $result = '<select name="Time_Hour">'.$n. $this->hours['none'] .$n.'</select>'
+            .$n.'<select name="Time_Minute">'.$n. $this->minutes['none'] .$n.'</select>'
+            .$n.'<select name="Time_Second">'.$n.'<option value=""></option>'.$n. $this->seconds['none'] .$n.'</select>';
+        $tpl = $this->smarty->createTemplate('eval:{html_select_time time=null second_empty=""}');
+        $this->assertEquals($result, $this->smarty->fetch($tpl));
+        
+        $result = '<select name="Time_Hour">'.$n.'<option value="">hour</option>'.$n. $this->hours['none'] .$n.'</select>'
+            .$n.'<select name="Time_Minute">'.$n.'<option value="">minute</option>'.$n. $this->minutes['none'] .$n.'</select>'
+            .$n.'<select name="Time_Second">'.$n.'<option value="">second</option>'.$n. $this->seconds['none'] .$n.'</select>';
+        $tpl = $this->smarty->createTemplate('eval:{html_select_time time=null hour_empty="hour" minute_empty="minute" second_empty="second"}');
         $this->assertEquals($result, $this->smarty->fetch($tpl));
     }
    
