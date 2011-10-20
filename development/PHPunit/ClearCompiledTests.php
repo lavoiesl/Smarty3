1c1d59e9cb16c7e2538707c5d4e277c5d2d93963
rodneyrehm <rodneyrehm@9dce5a81-9a42-0410-99e4-3799d3902e7f>
2011-10-20 12:50:12 +0000

    - bugfix Smarty::clearCompiledTemplate() threw an Exception if $cache_id was not present in $compile_dir when $use_sub_dirs = true. -rewrote ClearCompiledTests to cover all possible usage scenarios

--- development/PHPunit/ClearCompiledTests.php
+++ development/PHPunit/ClearCompiledTests.php
@@ -4,6 +4,7 @@
 *
 * @package PHPunit
 * @author Uwe Tews
+* @author Rodney Rehm
 */
 
 
@@ -15,7 +16,6 @@ class ClearCompiledTests extends PHPUnit_Framework_TestCase {
     public function setUp()
     {
         $this->smarty = SmartyTests::$smarty;
-        $this->smartyBC = SmartyTests::$smartyBC;
         SmartyTests::init();
     }
 
@@ -24,127 +24,355 @@ class ClearCompiledTests extends PHPUnit_Framework_TestCase {
         return true;
     }
 
+    // helpers
     /**
-    * test clearCompiledTemplate method for all files
-    */
-    public function testClearCompiledAll()
+     * clear $smarty->compile_dir
+     *
+     * @return void
+     */
+    protected function clearFiles()
     {
-        $this->smarty->clearCompiledTemplate();
-        file_put_contents($this->smarty->getCompileDir() . 'dummy.php', 'test');
-        file_put_contents($this->smarty->getCompileDir() . 'dummy2.php', 'test');
-        $this->assertEquals(2, $this->smarty->clearCompiledTemplate());
-       	$this->assertFalse(file_exists($this->smarty->getCompileDir() . 'dummy.php'));
-        $this->assertFalse(file_exists($this->smarty->getCompileDir() . 'dummy2.php'));
-    }
-    public function testSmarty2ClearCompiledAll()
-    {
-        $this->smartyBC->clearCompiledTemplate();
-        file_put_contents($this->smartyBC->getCompileDir() . 'dummy.php', 'test');
-        file_put_contents($this->smartyBC->getCompileDir() . 'dummy2.php', 'test');
-        $this->smartyBC->clear_compiled_tpl();
-        $this->assertFalse(file_exists($this->smartyBC->getCompileDir() . 'dummy.php'));
-        $this->assertFalse(file_exists($this->smartyBC->getCompileDir() . 'dummy2.php'));
-    }
-    /**
-    * test clearCompiledTemplate method for a specific resource
-    */
-    public function testClearCompiledResource()
-    {
-        $this->smarty->clearCompiledTemplate();
-        file_put_contents($this->smarty->getCompileDir() . sha1($this->smarty->getTemplateDir(0) . 'helloworld.tpl').'.file.helloworld.tpl.php', 'test');
-        file_put_contents($this->smarty->getCompileDir() . sha1($this->smarty->getTemplateDir(0) . 'helloworld.tpl').'.file.helloworld.tpl.cache.php', 'test');
-        file_put_contents($this->smarty->getCompileDir() . 'dummy2.php', 'test');
-        $this->assertEquals(2, $this->smarty->clearCompiledTemplate('helloworld.tpl'));
-        $this->assertFalse(file_exists($this->smarty->getCompileDir() . sha1($this->smarty->getTemplateDir(0) . 'helloworld.tpl').'.file.helloworld.tpl.php'));
-        $this->assertFalse(file_exists($this->smarty->getCompileDir() . sha1($this->smarty->getTemplateDir(0) . 'helloworld.tpl').'.file.helloworld.tpl.cache.php'));
-        $this->assertTrue(file_exists($this->smarty->getCompileDir() . 'dummy2.php'));
-    }
-    public function testSmarty2ClearCompiledResource()
-    {
-        $this->smartyBC->clearCompiledTemplate();
-        file_put_contents($this->smartyBC->getCompileDir() . sha1($this->smartyBC->getTemplateDir(0) . 'helloworld.tpl').'.file.helloworld.tpl.php', 'test');
-        file_put_contents($this->smartyBC->getCompileDir() . sha1($this->smartyBC->getTemplateDir(0) . 'helloworld.tpl').'.file.helloworld.tpl.cache.php', 'test');
-        file_put_contents($this->smartyBC->getCompileDir() . 'dummy2.php', 'test');
-        $this->smartyBC->clear_compiled_tpl('helloworld.tpl');
-        $this->assertFalse(file_exists($this->smartyBC->getCompileDir() . sha1($this->smartyBC->getTemplateDir(0) . 'helloworld.tpl').'.file.helloworld.tpl.php'));
-        $this->assertFalse(file_exists($this->smartyBC->getCompileDir() . sha1($this->smartyBC->getTemplateDir(0) . 'helloworld.tpl').'.file.helloworld.tpl.cache.php'));
-        $this->assertTrue(file_exists($this->smartyBC->getCompileDir() . 'dummy2.php'));
+        $directory = realpath($this->smarty->getCompileDir());
+        if (!$directory) {
+            return;
+        }
+        
+        $di = new RecursiveDirectoryIterator($directory);
+        // $it = new RecursiveIteratorIterator($di, RecursiveIteratorIterator::CHILD_FIRST | FilesystemIterator::SKIP_DOTS);
+        $it = new RecursiveIteratorIterator($di, RecursiveIteratorIterator::CHILD_FIRST);
+        foreach ($it as $file) {
+            $_file = $file->__toString();
+
+            if (preg_match("#[\\/]\.#", $_file)) {
+                continue;
+            }
+
+			if ($file->isDir()) {
+				rmdir($_file);
+			} else {
+				unlink($_file);
+			}
+
+        }
     }
     /**
-    * test clearCompiledTemplate method not expired
-    */
-    public function testClearCompiledNotExpired()
+     * list of compiled files
+     * @var array
+     */
+    protected $_files = array();
+    /**
+     * generate compiled files
+     * @uses $_files to store references
+     * @return array list of files array( id => path )
+     */
+    protected function makeFiles()
     {
-        $this->smarty->clearCompiledTemplate();
-        file_put_contents($this->smarty->getCompileDir() . 'dummy.php', 'test');
-        touch($this->smarty->getCompileDir() . 'dummy.php', time()-1000);
-        $this->assertEquals(0, $this->smarty->clearCompiledTemplate(null, null, 2000));
-        $this->assertTrue(file_exists($this->smarty->getCompileDir() . 'dummy.php'));
-    }
-    public function testSnarty2ClearCompiledNotExpired()
-    {
-        $this->smartyBC->clearCompiledTemplate();
-        file_put_contents($this->smartyBC->getCompileDir() . 'dummy.php', 'test');
-        touch($this->smartyBC->getCompileDir() . 'dummy.php', time()-1000);
-        $this->smartyBC->clear_compiled_tpl(null, null, 2000);
-        $this->assertTrue(file_exists($this->smartyBC->getCompileDir() . 'dummy.php'));
+        $this->_files = array();
+        $directory_length = strlen($this->smarty->getCompileDir());
+        $templates = array(
+            'helloworld.tpl' => array(null, 'compile1', 'compile2'),
+            'helloworld2.tpl' => array(null, 'compile1', 'compile2'),
+        );
+
+        foreach ($templates as $template => $compile_ids) {
+            foreach ($compile_ids as $compile_id) {
+                $tpl = $this->smarty->createTemplate($template, null, $compile_id);
+                $tpl->fetch();
+                $this->_files[$template . '#' . $compile_id] = substr($tpl->compiled->filepath, $directory_length - 1);
+            }
+        }
+        
+        Smarty_Resource::$sources = array();
+        $this->smarty->template_objects = array();
+        
+        return $this->_files;
     }
     /**
-    * test clearCompiledTemplate method expired
-    */
-    public function testClearCompiledExpired()
+     * Transform $id to $path
+     *
+     * @param array $keys IDs like "template#compile_id"
+     * @return array list of (sorted) compiled file paths
+     */
+    protected function expectFiles($keys)
     {
-        $this->smarty->clearCompiledTemplate();
-        file_put_contents($this->smarty->getCompileDir() . 'dummy.php', 'test');
-        touch($this->smarty->getCompileDir() . 'dummy.php', time()-1000);
-        $this->assertEquals(1, $this->smarty->clearCompiledTemplate(null, null, 500));
-        $this->assertFalse(file_exists($this->smarty->getCompileDir() . 'dummy.php'));
-    }
-    public function testSmarty2ClearCompiledExpired()
-    {
-        $this->smartyBC->clearCompiledTemplate();
-        file_put_contents($this->smartyBC->getCompileDir() . 'dummy.php', 'test');
-        touch($this->smartyBC->getCompileDir() . 'dummy.php', time()-1000);
-        $this->smartyBC->clear_compiled_tpl(null, null, 500);
-        $this->assertFalse(file_exists($this->smartyBC->getCompileDir() . 'dummy.php'));
+        $files = array();
+        foreach ($keys as $key) {
+            if (isset($this->_files[$key])) {
+                $files[] = $this->_files[$key];
+            }
+        }
+        sort($files);
+        return $files;
     }
     /**
-    * test clearCompiledTemplate with compile_id
-    */
-    public function testClearCompiledCompileId()
+     * update mtime of compiled files
+     *
+     * @param array $keys IDs like "template#compile_id"
+     * @param string $offset time offset added to time()
+     * @return void
+     */
+    protected function touchFiles($keys, $offset=0)
     {
-        $this->smarty->use_sub_dirs = true;
-        $tpl = $this->smarty->createTemplate('helloworld.tpl', null, 'blar');
-        Smarty_Internal_Write_File::writeFile($tpl->compiled->filepath, 'hello world', $this->smarty);
-        $tpl2 = $this->smarty->createTemplate('helloworld.tpl', null, 'blar2');
-        Smarty_Internal_Write_File::writeFile($tpl2->compiled->filepath, 'hello world', $this->smarty);
-        $tpl3 = $this->smarty->createTemplate('helloworld2.tpl', null, 'blar');
-        Smarty_Internal_Write_File::writeFile($tpl3->compiled->filepath, 'hello world', $this->smarty);
-        $this->assertTrue(file_exists($tpl->compiled->filepath));
-        $this->assertTrue(file_exists($tpl2->compiled->filepath));
-        $this->assertTrue(file_exists($tpl3->compiled->filepath));
-        $this->assertEquals(2, $this->smarty->clearCompiledTemplate (null, 'blar'));
-        $this->assertFalse(file_exists($tpl->compiled->filepath));
-        $this->assertTrue(file_exists($tpl2->compiled->filepath));
-        $this->assertFalse(file_exists($tpl3->compiled->filepath));
+        $base = rtrim($this->smarty->getCompileDir(), "\\/");
+        $time = time();
+        foreach ($keys as $key) {
+            if (isset($this->_files[$key])) {
+                touch($base . $this->_files[$key], $time + $offset);
+            }
+        }
     }
-    public function testSmarty2ClearCompiledCompileId()
+    /**
+     * find all compiled files
+     *
+     * @return array list of (sorted) compiled file paths
+     */
+    protected function getFiles()
     {
-        $this->smartyBC->use_sub_dirs = true;
-        $tpl = $this->smartyBC->createTemplate('helloworld.tpl', null, 'blar');
-        Smarty_Internal_Write_File::writeFile($tpl->compiled->filepath, 'hello world', $this->smarty);
-        $tpl2 = $this->smartyBC->createTemplate('helloworld.tpl', null, 'blar2');
-        Smarty_Internal_Write_File::writeFile($tpl2->compiled->filepath, 'hello world', $this->smarty);
-        $tpl3 = $this->smartyBC->createTemplate('helloworld2.tpl', null, 'blar');
-        Smarty_Internal_Write_File::writeFile($tpl3->compiled->filepath, 'hello world', $this->smarty);
-        $this->assertTrue(file_exists($tpl->compiled->filepath));
-        $this->assertTrue(file_exists($tpl2->compiled->filepath));
-        $this->assertTrue(file_exists($tpl3->compiled->filepath));
-        $this->smartyBC->clear_compiled_tpl(null, 'blar');
-        $this->assertFalse(file_exists($tpl->compiled->filepath));
-        $this->assertTrue(file_exists($tpl2->compiled->filepath));
-        $this->assertFalse(file_exists($tpl3->compiled->filepath));
+        $directory = realpath($this->smarty->getCompileDir());
+        if (!$directory) {
+            return array();
+        }
+
+        $directory_length = strlen($directory);
+        $files = array();
+
+        $di = new RecursiveDirectoryIterator($directory);
+        $it = new RecursiveIteratorIterator($di);
+        foreach ($it as $file) {
+            $_file = $file->__toString();
+            // skip anything with a /. in it.
+            if (preg_match("#[\\/]\.#", $_file) || !$file->isFile()) {
+                continue;
+            }
+
+			$files[] = substr($file->__toString(), $directory_length);
+        }
+        sort($files);
+        return $files;
     }
+    
+
+    // Smarty::clearCompiledTemplate(null, null, null)
+    public function testClearAll()
+    {
+        $this->runClearAll(false);
+    }
+    public function testSubsClearAll()
+    {
+        $this->runClearAll(true);
+    }
+    public function runClearAll($use_sub_dirs)
+    {
+        $this->smarty->use_sub_dirs = $use_sub_dirs;
+        $this->clearFiles();
+        $this->makeFiles();
+        
+        $expected = array();
+        $this->assertEquals(6, $this->smarty->clearCompiledTemplate());
+
+        $this->assertEquals($this->expectFiles($expected), $this->getFiles());
+        $this->clearFiles();
+    }
+    
+    // Smarty::clearCompiledTemplate($template, null, null)
+    public function testClearTemplate()
+    {
+        $this->runClearTemplate(false);
+    }
+    public function testSubsClearTemplate()
+    {
+        $this->runClearTemplate(true);
+    }
+    public function testClearOtherTemplate()
+    {
+        $this->runClearOtherTemplate(false);
+    }
+    public function testSubsClearOtherTemplate()
+    {
+        $this->runClearOtherTemplate(true);
+    }
+    public function runClearTemplate($use_sub_dirs)
+    {
+        $this->smarty->use_sub_dirs = $use_sub_dirs;
+        $this->clearFiles();
+        $this->makeFiles();
+        
+        $expected = array('helloworld2.tpl#', 'helloworld2.tpl#compile1', 'helloworld2.tpl#compile2');
+        $this->assertEquals(3, $this->smarty->clearCompiledTemplate('helloworld.tpl'));
+
+        $this->assertEquals($this->expectFiles($expected), $this->getFiles());
+        $this->clearFiles();
+    }
+    public function runClearOtherTemplate($use_sub_dirs)
+    {
+        $this->smarty->use_sub_dirs = $use_sub_dirs;
+        $this->clearFiles();
+        $this->makeFiles();
+        
+        $expected = array_keys($this->_files);
+        $this->assertEquals(0, $this->smarty->clearCompiledTemplate('foobar.tpl'));
+        
+        $this->assertEquals($this->expectFiles($expected), $this->getFiles());
+        $this->clearFiles();
+    }
+    
+    // Smarty::clearCompiledTemplate(null, $cache_id, null)
+    public function testClearCompileid()
+    {
+        $this->runClearCompileid(false);
+    }
+    public function testSubsClearCompileid()
+    {
+        $this->runClearCompileid(true);
+    }
+    public function testClearOtherCompileid()
+    {
+        $this->runClearOtherCompileid(false);
+    }
+    public function testSubsClearOtherCompileid()
+    {
+        $this->runClearOtherCompileid(true);
+    }
+    public function runClearCompileid($use_sub_dirs)
+    {
+        $this->smarty->use_sub_dirs = $use_sub_dirs;
+        $this->clearFiles();
+        $this->makeFiles();
+        
+        $expected = array('helloworld.tpl#', 'helloworld2.tpl#', 'helloworld.tpl#compile2', 'helloworld2.tpl#compile2');
+        $this->assertEquals(2, $this->smarty->clearCompiledTemplate(null, 'compile1'));
+
+        $this->assertEquals($this->expectFiles($expected), $this->getFiles());
+        $this->clearFiles();
+        
+    }
+    public function runClearOtherCompileid($use_sub_dirs)
+    {
+        $this->smarty->use_sub_dirs = $use_sub_dirs;
+        $this->clearFiles();
+        $this->makeFiles();
+        
+        $expected = array_keys($this->_files);
+        $this->assertEquals(0, $this->smarty->clearCompiledTemplate(null, 'other'));
+        
+        $this->assertEquals($this->expectFiles($expected), $this->getFiles());
+        $this->clearFiles();
+    }
+    
+    // Smarty::clearCompiledTemplate(null, null, $expired)
+    public function testClearExpired()
+    {
+        $this->runClearExpired(false);
+    }
+    public function testSubsClearExpired()
+    {
+        $this->runClearExpired(true);
+    }
+    public function runClearExpired($use_sub_dirs)
+    {
+        $this->smarty->use_sub_dirs = $use_sub_dirs;
+        $this->clearFiles();
+        $this->makeFiles();
+        
+        $expected = array('helloworld.tpl#', 'helloworld2.tpl#');
+        $this->touchFiles(array_diff(array_keys($this->_files), $expected), -1000);
+        $this->assertEquals(4, $this->smarty->clearCompiledTemplate(null, null, 500));
+
+        $this->assertEquals($this->expectFiles($expected), $this->getFiles());
+        $this->clearFiles();
+    }
+
+    // Smarty::clearCompiledTemplate($template, null, $expired)
+    public function testClearTemplateExpired()
+    {
+        $this->runClearTemplateExpired(false);
+    }
+    public function testSubsClearTemplateExpired()
+    {
+        $this->runClearTemplateExpired(true);
+    }
+    public function runClearTemplateExpired($use_sub_dirs)
+    {
+        $this->smarty->use_sub_dirs = $use_sub_dirs;
+        $this->clearFiles();
+        $this->makeFiles();
+
+        $expected = array('helloworld.tpl#', 'helloworld.tpl#compile2', 'helloworld2.tpl#', 'helloworld2.tpl#compile1', 'helloworld2.tpl#compile2');
+        $this->touchFiles(array('helloworld.tpl#compile1'), -1000);
+        $this->assertEquals(1, $this->smarty->clearCompiledTemplate("helloworld.tpl", null, 500));
+
+        $this->assertEquals($this->expectFiles($expected), $this->getFiles());
+        $this->clearFiles();
+    }
+    
+    // Smarty::clearCompiledTemplate($template, $cache_id, $expired)
+    public function testClearTemplateCacheidExpired()
+    {
+        $this->runClearTemplateCacheidExpired(false);
+    }
+    public function testSubsClearTemplateCacheidExpired()
+    {
+        $this->runClearTemplateCacheidExpired(true);
+    }
+    public function runClearTemplateCacheidExpired($use_sub_dirs)
+    {
+        $this->smarty->use_sub_dirs = $use_sub_dirs;
+        $this->clearFiles();
+        $this->makeFiles();
+
+        $expected = array('helloworld.tpl#', 'helloworld.tpl#compile2', 'helloworld2.tpl#', 'helloworld2.tpl#compile1', 'helloworld2.tpl#compile2');
+        $this->touchFiles(array('helloworld.tpl#compile1', 'helloworld.tpl#compile2'), -1000);
+        $this->assertEquals(1, $this->smarty->clearCompiledTemplate("helloworld.tpl", "compile1", 500));
+
+        $this->assertEquals($this->expectFiles($expected), $this->getFiles());
+        $this->clearFiles();
+    }
+    
+    // Smarty::clearCompiledTemplate(null, $cache_id, $expired)
+    public function testClearCacheidExpired()
+    {
+        $this->runClearCacheidExpired(false);
+    }
+    public function testSubsClearCacheidExpired()
+    {
+        $this->runClearCacheidExpired(true);
+    }
+    public function runClearCacheidExpired($use_sub_dirs)
+    {
+        $this->smarty->use_sub_dirs = $use_sub_dirs;
+        $this->clearFiles();
+        $this->makeFiles();
+
+        $expected = array('helloworld.tpl#', 'helloworld.tpl#compile2', 'helloworld2.tpl#', 'helloworld2.tpl#compile1', 'helloworld2.tpl#compile2');
+        $this->touchFiles(array('helloworld.tpl#compile1'), -1000);
+        $this->assertEquals(1, $this->smarty->clearCompiledTemplate(null, "compile1", 500));
+
+        $this->assertEquals($this->expectFiles($expected), $this->getFiles());
+        $this->clearFiles();
+    }
+    
+    // Smarty::clearCompiledTemplate($template, $cache_id, null)
+    public function testClearTemplateCacheid()
+    {
+        $this->runClearTemplateCacheid(false);
+    }
+    public function testSubsClearTemplateCacheid()
+    {
+        $this->runClearTemplateCacheid(true);
+    }
+    public function runClearTemplateCacheid($use_sub_dirs)
+    {
+        $this->smarty->use_sub_dirs = $use_sub_dirs;
+        $this->clearFiles();
+        $this->makeFiles();
+
+        $expected = array('helloworld.tpl#', 'helloworld.tpl#compile2', 'helloworld2.tpl#', 'helloworld2.tpl#compile1', 'helloworld2.tpl#compile2');
+        $this->assertEquals(1, $this->smarty->clearCompiledTemplate("helloworld.tpl", "compile1"));
+
+        $this->assertEquals($this->expectFiles($expected), $this->getFiles());
+        $this->clearFiles();
+    }
+    
 }
 
 ?>
